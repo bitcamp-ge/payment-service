730250b5ad811ec518e4eba8919f4ce1140c2ea0
NickKipshidze <kipshidze.nick@gmail.com>
2024-03-11 15:45:32 +0400

    Add LOGFILE_PATH & DEBUGFILE_PATH variables & ignore logfiles in .gitignore

--- settings.py
+++ settings.py
@@ -2,6 +2,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+LOGFILE_PATH = os.environ.get("LOGFILE_PATH")
+DEBUGFILE_PATH = os.environ.get("DEBUGFILE_PATH")
+
 BACKEND_URL = os.environ.get("BACKEND_URL").strip("/")
 
 DJANGO_ADMIN_USERNAME = os.environ.get("DJANGO_ADMIN_USERNAME")
